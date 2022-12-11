4b0ba59070899f01bd36170eddc6915d3e28febf
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2022-12-11 09:39:09 -0500

    Add user input. Format user input. Formated user input to capitalize first letter and return the rest lower case. Need to remove whitespace from inputs and verify that Rock, Paper, or Scissors was entered.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
+//test console
 console.log("Hello World");
-random = Math.floor(Math.random() * 3) + 1;
-computerSelection = randomJunken(random)
 
+//define random number to use for computerSelection
+random = Math.floor(Math.random() * 3) + 1;
+
+//assign computerSelection
+computerSelection = getComputerChoice(random)
+
+//check rng
 console.log(random)
 
-function randomJunken (random) {
+//creates computer's random rock paper or scissor choice
+function getComputerChoice (random) {
     if (random == 1) {
       computerSelection = "Rock"
     } else if (random == 2) {
@@ -14,4 +21,22 @@ function randomJunken (random) {
     return computerSelection
 }
 
-console.log(computerSelection)
+//requests player input
+playerSelection = prompt('Enter "Rock", "Paper", or "Scissors".')
+
+function capitalize (x) {
+    firstLetter = x.charAt(0);
+    capLetter = firstLetter.toUpperCase(0);
+    restWord = x.slice(1,);
+    lowLetters = restWord.toLowerCase();
+    newWord = capLetter.concat (lowLetters);
+    return newWord;
+  }
+
+  function validatePlayerSelection ()
+
+
+
+
+console.log(computerSelection);
+console.log(capitalize(playerSelection));
